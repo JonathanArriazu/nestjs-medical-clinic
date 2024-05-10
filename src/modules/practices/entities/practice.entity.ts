6f117718b2b5565533c035f333c4c4ed58e4a68e
Jonathan Arriazu <joni.arriazu2@gmail.com>
2024-05-10 13:08:20 -0300

    practice entity added

--- src/modules/practices/entities/practice.entity.ts
+++ src/modules/practices/entities/practice.entity.ts
@@ -1,1 +1,14 @@
-export class Practice {}
+import { BaseEntity } from "src/config/database/base.entity";
+import { Column, Entity } from "typeorm";
+
+@Entity()
+export class Practice extends BaseEntity {
+    @Column()
+    Duration: string;
+
+    @Column()
+    Complications: string;  
+
+    @Column()
+    Final_result: string;
+}
--- src/modules/practices/practices.module.ts
+++ src/modules/practices/practices.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PracticesService } from './practices.service';
 import { PracticesController } from './practices.controller';
+import { Practice } from './entities/practice.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Practice])],
   controllers: [PracticesController],
   providers: [PracticesService],
 })
