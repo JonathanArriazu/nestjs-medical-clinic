e5955237695f133626f2fa8fbf832fc667261f07
Jonathan Arriazu <joni.arriazu2@gmail.com>
2024-05-10 13:07:24 -0300

    medical history entity added

--- src/modules/medical_histories/entities/medical_history.entity.ts
+++ src/modules/medical_histories/entities/medical_history.entity.ts
@@ -1,5 +1,7 @@
 import { BaseEntity } from "src/config/database/base.entity";
+import { Entity } from "typeorm";
 
+@Entity()
 export class MedicalHistory extends BaseEntity {
 
 }
--- src/modules/medical_histories/medical_histories.module.ts
+++ src/modules/medical_histories/medical_histories.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MedicalHistoriesService } from './medical_histories.service';
 import { MedicalHistoriesController } from './medical_histories.controller';
+import { MedicalHistory } from './entities/medical_history.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([MedicalHistory])],
   controllers: [MedicalHistoriesController],
   providers: [MedicalHistoriesService],
 })
