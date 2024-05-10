2a884aa2661e78188473dc44a2c77e364a474b22
Jonathan Arriazu <joni.arriazu2@gmail.com>
2024-05-10 12:28:58 -0300

    connection to database

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,10 +1,24 @@
-import { Module } from '@nestjs/common';
+import { Logger, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { databaseConfig } from 'src/config/database/data.base';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot(databaseConfig),
+    ConfigModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+
+  private readonly logger = new Logger('AppModule');
+
+  constructor() {
+    this.logger.log('AppModule initialized');
+  }
+
+}
