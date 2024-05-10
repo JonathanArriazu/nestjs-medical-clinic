fa017cfe600cef7a422a75b0373175ca82d4df68
Jonathan Arriazu <joni.arriazu2@gmail.com>
2024-05-10 13:08:37 -0300

    desease entity added

--- src/modules/deseases/entities/desease.entity.ts
+++ src/modules/deseases/entities/desease.entity.ts
@@ -1,1 +1,10 @@
-export class Desease {}
+import { BaseEntity } from "src/config/database/base.entity";
+import { Column, Entity } from "typeorm";
+
+@Entity()
+export class Desease extends BaseEntity{
+
+    @Column()
+    name: string;
+
+}
