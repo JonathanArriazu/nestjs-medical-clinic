6300c003cff1dd77358154f51384d1b94222d1fb
Jonathan Arriazu <joni.arriazu2@gmail.com>
2024-05-10 13:07:55 -0300

    doctor entity added

--- src/modules/doctors/entities/doctor.entity.ts
+++ src/modules/doctors/entities/doctor.entity.ts
@@ -1,1 +1,18 @@
-export class Doctor {}
+import { BaseEntity } from "src/config/database/base.entity";
+import { Column, Entity } from "typeorm";
+
+@Entity()
+export class Doctor extends BaseEntity {
+
+    @Column()
+    License_number: number;
+
+    @Column()
+    Name: string;  
+
+    @Column()
+    Last_name: string;
+
+    @Column()
+    Entry_date: Date;
+}
