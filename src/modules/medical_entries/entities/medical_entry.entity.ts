f2cc906b6d1c6d0d11a6f788801b9545e9c6fa14
Jonathan Arriazu <joni.arriazu2@gmail.com>
2024-05-10 13:07:38 -0300

    medical entry entity added

--- src/modules/medical_entries/entities/medical_entry.entity.ts
+++ src/modules/medical_entries/entities/medical_entry.entity.ts
@@ -1,1 +1,10 @@
-export class MedicalEntry {}
+import { BaseEntity } from "src/config/database/base.entity";
+import { Column, Entity } from "typeorm";
+
+@Entity()
+export class MedicalEntry extends BaseEntity {
+
+    @Column()
+    Date: Date;
+
+}
--- src/modules/medical_entries/medical_entries.module.ts
+++ src/modules/medical_entries/medical_entries.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MedicalEntriesService } from './medical_entries.service';
 import { MedicalEntriesController } from './medical_entries.controller';
+import { MedicalEntry } from './entities/medical_entry.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([MedicalEntry])],
   controllers: [MedicalEntriesController],
   providers: [MedicalEntriesService],
 })
