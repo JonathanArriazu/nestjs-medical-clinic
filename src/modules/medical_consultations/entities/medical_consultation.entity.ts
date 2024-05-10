d7decdb97c005350886eac8c59cabd3a6ffbd01b
Jonathan Arriazu <joni.arriazu2@gmail.com>
2024-05-10 13:08:10 -0300

    medical consultation entity added

--- src/modules/medical_consultations/entities/medical_consultation.entity.ts
+++ src/modules/medical_consultations/entities/medical_consultation.entity.ts
@@ -1,1 +1,11 @@
-export class MedicalConsultation {}
+import { BaseEntity } from "src/config/database/base.entity";
+import { Column, Entity } from "typeorm";
+
+@Entity()
+export class MedicalConsultation extends BaseEntity{
+    @Column()
+    Consultation_reason: string;
+
+    @Column()
+    Is_confirmed: boolean;
+}
--- src/modules/medical_consultations/medical_consultations.module.ts
+++ src/modules/medical_consultations/medical_consultations.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MedicalConsultationsService } from './medical_consultations.service';
 import { MedicalConsultationsController } from './medical_consultations.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { MedicalConsultation } from './entities/medical_consultation.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([MedicalConsultation])],
   controllers: [MedicalConsultationsController],
   providers: [MedicalConsultationsService],
 })
