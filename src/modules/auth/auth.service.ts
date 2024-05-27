9e1573386af5d46070b4f7c7350b1bdc73646300
Jonathan Arriazu <joni.arriazu2@gmail.com>
2024-05-27 11:43:26 -0300

    fix: auth started using database users

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -10,14 +10,14 @@ export class AuthService {
   ) {}
 
   async signIn(
-    username: string,
+    email: string,
     pass: string,
   ): Promise<{ access_token: string }> {
-    const user = await this.usersService.findOne(username);
+    const user = await this.usersService.findOneUser(email);
     if (user?.password !== pass) {
       throw new UnauthorizedException();
     }
-    const payload = { id: user.userId, username: user.username };
+    const payload = { email: user.email, role: user.role };
     return {
       access_token: await this.jwtService.signAsync(payload),
     };
